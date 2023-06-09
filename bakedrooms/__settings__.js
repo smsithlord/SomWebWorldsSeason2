52bcde37f9aa0a0a43e55a540f2dc18a584f7566
Elijah Newman-Gomez <smsithlord@yahoo.com>
2023-06-09 13:24:46 -0700

    baked rooms

---
+++ bakedrooms/__settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1759101.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 137137117, 137137125, 137137127, 137137141, 137137144, 137137151, 137137153, 137137154 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/137137114/1/basis.wasm.js', 'wasmUrl' : 'files/assets/137137115/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/137137113/1/basis.js', 'preload' : false},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/137137110/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/137137111/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/137137109/1/ammo.js', 'preload' : true},
+];
