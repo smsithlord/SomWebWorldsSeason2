c3a060a4c905a89d7f25acdd71921d18603120b0
Elijah Newman-Gomez <smsithlord@yahoo.com>
2023-06-27 15:45:37 -0700

    initial commit

---
+++ curvyballs/__settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1783963.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 140208248, 140208256, 140208258, 140208272, 140208275, 140208282, 140208284, 140208285 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/140208245/1/basis.wasm.js', 'wasmUrl' : 'files/assets/140208246/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/140208244/1/basis.js', 'preload' : false},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/140208241/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/140208242/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/140208240/1/ammo.js', 'preload' : true},
+];
