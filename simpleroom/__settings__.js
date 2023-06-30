737f4f598128067d34fcc1f911e9c4ce8dc89132
Elijah Newman-Gomez <smsithlord@yahoo.com>
2023-06-30 11:02:09 -0700

    initial

---
+++ simpleroom/__settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1788106.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 140917424, 140917385, 140917416, 140917384, 140917393, 140917381, 140917401, 140917379 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/140917400/1/basis.wasm.js', 'wasmUrl' : 'files/assets/140917398/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/140917390/1/basis.js', 'preload' : false},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/140917394/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/140917404/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/140917403/1/ammo.js', 'preload' : true},
+];
