3db6df8722dbcb0ae246451cebee57af455fc811
Elijah Newman-Gomez <smsithlord@yahoo.com>
2023-09-29 10:13:07 -0700

    initial commit

---
+++ quest3land/__settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1868880.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 151958362, 151958370, 151958372, 151958386, 151958389, 151958396, 151958398, 151958399 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/151958359/1/basis.wasm.js', 'wasmUrl' : 'files/assets/151958360/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/151958358/1/basis.js', 'preload' : false},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/151958355/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/151958356/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/151958354/1/ammo.js', 'preload' : true},
+];
